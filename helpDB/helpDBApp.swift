a7eb83386883ddebd11eadf19017a0d0ff1964ec
Jonathan Breaker <jbreak23@gmail.com>
2025-05-09 13:55:54 -0400

    Initial Commit

---
+++ helpDB/helpDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  helpDBApp.swift
+//  helpDB
+//
+//  Created by Jonathan Breaker on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct helpDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
